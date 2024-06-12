cb50dfdc2d6ec2fcf0d8d11514d8b78020e46035
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-06-12 08:56:14 +0300

    changes

--- models/author.py
+++ models/author.py
@@ -1,44 +1,82 @@
+from database.connection import get_db_connection
+
 class Author:
-    def __init__(self, id, name):
+    def _init_(self, id, name):
+        # Initialize an Author object with id and name
         self.id = id
+        # Set the name of the author
         self.name = name
-        self.cursor = self.connection.cursor()
 
-        if not self.name:
-            raise ValueError("Author name must be specified")
-        try:
-            self.cursor.execute('INSERT INTO authors (name) VALUES (?)', (self.name,))
-            self.connection.commit()
-            self.id = self.cursor.lastrowid
-        except Exception as e:
-            self.connection.rollback()
-            print (e)
-            raise e
-
-    def __repr__(self):
-        return f'<Author {self.name}>'
-    
     @property
-    def name (self):
-        return self.name
-    @ name.setter
-    def name (self, value):
-        if not hasattr(self, '_name'):
-            self._name = value
+    def id(self):
+        # Getter for the id property
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        # Setter for the id property, ensuring it's an integer
+        if not isinstance(value, int):
+            raise ValueError("Author ID must be of type int")
+        self._id = value
+
+    @property
+    def name(self):
+        # Getter for the name property
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # Setter for the name property, ensuring it's a non-empty string
+        if not isinstance(value, str):
+            raise ValueError("Author name must be a string")
+        if len(value) == 0:
+            raise ValueError("Author name must not be empty")
+        self._name = value
+
+    @staticmethod
+    def create(name):
+        # Create a new Author object and insert it into the database
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        # Insert the author's name into the database
+        cursor.execute("INSERT INTO authors (name) VALUES (?)", (name,))
+        
+        # Commit the changes and retrieve the inserted author's ID
+        conn.commit()
+        
+        # Get the ID of the newly created author
+        author_id = cursor.lastrowid
+        
+        # Close the database connection
+        conn.close()
+        
+        # Return a new Author object with the inserted author's ID and name
+        return Author(author_id, name)
 
     def articles(self):
-        self.cursor.execute("""
-            SELECT a.title, m.name
-            FROM articles a
-            JOIN authors aut ON a.author_id = aut.id
-            JOIN magazines m ON a.magazine_id = m.id
-            WHERE aut.id = %s
-        """, (self.id,))
-        return self.cursor.fetchall()
-    
-    def __delattr__(self):
-        self.cursor.close()
-        self.connection.close()
-      
+        # Retrieve all articles associated with this author from the database
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        # Retrieve all articles associated with this author from the 
+        # Query the database for articles with the current author's ID
+        cursor.execute("SELECT * FROM articles WHERE author_id=?", (self.id,))
+        
+        articles = cursor.fetchall()
+        conn.close()
+        return articles
 
-           
+    def magazines(self):
+        # Retrieve all magazines associated with this author from the database
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT DISTINCT magazines.* FROM magazines INNER JOIN articles ON magazines.id = articles.magazine_id WHERE articles.author_id=?", (self.id,))
+        magazines = cursor.fetchall()
+        conn.close()
+        return magazines
+
+    def _repr_(self):
+        # Return a string representation of the Author object.
+        return f'<Author {self.name}>'
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,100 @@
+from database.connection import get_db_connection
+from models.author import Author
+
 class Magazine:
-    def __init__(self, id, name, category):
+    def _init_(self, id, name, category):
+        # Initialize a Magazine object with id, name, and category
         self.id = id
         self.name = name
         self.category = category
 
-    def __repr__(self):
-        return f'<Magazine {self.name}>'
+    @property
+    def id(self):
+        # Getter for the id property
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        # Setter for the id property, ensuring it's an integer
+        if not isinstance(value, int):
+            raise ValueError("Magazine ID must be of type int")
+        self._id = value
+
+    @property
+    def name(self):
+        # Getter for the name property
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # Setter for the name property, ensuring it's a string between 2 and 16 characters
+        if not isinstance(value, str):
+            raise ValueError("Magazine name must be a string")
+        if not (2 <= len(value) <= 16):
+            raise ValueError(
+                "Magazine name must be between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        # Getter for the category property
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        # Setter for the category property, ensuring it's a non-empty string
+        if not isinstance(value, str):
+            raise ValueError("Magazine category must be a string")
+        if len(value) == 0:
+            raise ValueError("Magazine category must not be empty")
+        self._category = value
+
+    @staticmethod
+    def create(name, category):
+        # Create a new Magazine object and insert it into the database
+        # Establish a database connection
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        # Insert the magazine into the database
+        cursor.execute(
+            "INSERT INTO magazines (name, category) VALUES (?, ?)", (name, category))
+        
+        # Commit the changes and retrieve the inserted magazine's ID
+        conn.commit()
+        magazine_id = cursor.lastrowid
+        
+        # Close the database connection
+        conn.close()
+        
+        # Return a new Magazine object with the inserted magazine's ID, name and category
+        return Magazine(magazine_id, name, category)
+
+    def articles(self):
+        # Retrieve all articles associated with this magazine from the database
+        # Establish a database connection
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        # Retrieve all articles associated with this magazine from the database
+        cursor.execute(
+            "SELECT * FROM articles WHERE magazine_id=?", (self.id,))
+        
+        # Fetch all retrieved articles
+        # Retrieve all articles associated with this magazine from the database
+        articles = cursor.fetchall()
+        
+        # Close the database connection
+        conn.close()
+        
+        # Return all articles associated with this magazine from the database
+        return articles
+
+    def contributors(self):
+        # Retrieve all distinct authors who have contributed to this magazine from the database
+        # Establish a database connection
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        # Retrieve all distinct authors who have contributed to this magazine from the database
+        cursor.execute(
+            "SELECT DISTINCT authors.* FROM authors INNER JOIN articles ON authors.id = articles.author_id WHERE articles.magazine_id=?", (self.id,))
